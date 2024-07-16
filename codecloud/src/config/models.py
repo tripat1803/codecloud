9161980da13ba975f460fa68c37901f2b7bf2636
TanishTaneja <thetanutaneja@gmail.com>
2024-07-16 22:50:11 +0530

    mysql tables created

--- codecloud/src/config/models.py
+++ codecloud/src/config/models.py
@@ -1,5 +1,29 @@
 from db import mycursor
 
-mycursor.execute('''CREATE TABLE _users ()''')
-mycursor.execute('''CREATE TABLE _projects ()''')
-mycursor.execute('''CREATE TABLE _deployments ()''')
+mycursor.execute('''CREATE TABLE _users (
+                    id int NOT NULL AUTO_INCREMENT,
+                    providerId varchar(255) NOT NULL,
+                    password varchar(255),
+                    provider varchar(255),
+                    role varchar(255),
+                    PRIMARY KEY(id)
+                 )''')
+
+mycursor.execute('''CREATE TABLE _projects (
+                    id int NOT NULL AUTO_INCREMENT,
+                    userId int,
+                    projectName varchar(255) NOT NULL,
+                    githubUrl varchar(255) NOT NULL,
+                    FOREIGN KEY(userId) REFERENCES _users(id),
+                    PRIMARY KEY(id)
+                 )''')
+
+mycursor.execute('''CREATE TABLE _deployments (
+                    id int NOT NULL AUTO_INCREMENT,
+                    projectId int,
+                    userId int,
+                    status varchar(255),
+                    FOREIGN KEY(projectId) REFERENCES _projects(id),
+                    FOREIGN KEY(userId) REFERENCES _users(id),
+                    PRIMARY KEY(id)
+                 )''')
